ca95db1d624d4b081793f3cb1ade9a6f4cd3c4d3
Sartajkhan228 <khanalam7328@gmail.com>
2025-12-29 00:22:21 +0500

    added the spinner

--- src/components/loader.tsx
+++ src/components/loader.tsx
@@ -1,13 +1,17 @@
+
+
 const Loader = () => {
+
     return (
 
-        <div className="border flex items-center justify-center min-h-screen">
+        <div className="bg-red-300 flex items-center justify-center min-h-screen">
             <div className="h-12 w-12 animate-spin rounded-full border-4
-        border-t-red-500
-        border-r-yellow-400
-        border-b-green-500
-        border-l-blue-500">
+                border-t-red-500
+                border-r-yellow-400
+                border-b-green-500
+                border-l-blue-500">
             </div>
+            <h1 className="border bg-red-500">HI</h1>
         </div>
     );
 };
